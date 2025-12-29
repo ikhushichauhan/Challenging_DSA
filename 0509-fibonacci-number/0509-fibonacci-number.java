def9be3684e64718fedac4e3fddb93150fd47f64
Khushi Chauhan <2401201167@krmu.edu.in>
2025-12-29 19:17:54 +0530

    Time: 0 ms (100%), Space: 42.2 MB (11.78%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else{
+
+            int firstnum = 0;
+            int secondnum = 1;
+
+            for(int i=1; i<=n; i++){
+                int thirdnum = firstnum + secondnum;
+                firstnum = secondnum;
+                secondnum = thirdnum;
+            }
+            return firstnum;
+        }
+        
+    }
+}
