4e2c14cf5514ba2a874832c17629d1c982bfa4a5
Khushi Chauhan <2401201167@krmu.edu.in>
2026-01-03 15:23:43 +0530

    Time: 3 ms (93.85%), Space: 42.4 MB (35.38%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,17 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int numOfWays(int n) {
+        long aba = 6;
+        long abc = 6;
+
+        for (int i = 2; i <= n; i++) {
+            long newAba = (aba * 3 + abc * 2) % MOD;
+            long newAbc = (aba * 2 + abc * 2) % MOD;
+            aba = newAba;
+            abc = newAbc;
+        }
+
+        return (int) ((aba + abc) % MOD);
+    }
+}
