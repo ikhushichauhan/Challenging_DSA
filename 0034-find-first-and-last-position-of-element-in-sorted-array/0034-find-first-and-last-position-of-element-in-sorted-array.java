4ac5d40b9c4fdbcc682c2690c3ad6099aca5f4e2
Khushi Chauhan <2401201167@krmu.edu.in>
2026-02-26 19:45:43 +0530

    Time: 0 ms (100%), Space: 48.5 MB (11.95%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,36 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false);
+        result[0] = left;
+        result[1] = right;
+        return result;        
+    }
 
-        int ans[] = {-1,-1};
-        if(nums.length==0){
-            return ans;
-        }
-        ans[0]=firstOccur(nums,target);
-        ans[1]=lastOccur(nums,target);
-        return ans;
-        
-    }
-    public int firstOccur(int[] nums, int target){
-        int start = 0;
-        int end = nums.length-1;
-        int ans = -1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(target==nums[mid]){
-                ans = mid;
-                end = mid-1;
-            }
-            else if(target>nums[mid]){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
+    private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
+        int left = 0;
+        int right = nums.length - 1;
+        int idx = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                idx = mid;
+                if (isSearchingLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
-        return ans;
+
+        return idx;
     }
-        public int lastOccur(int[] nums, int target){
-        int start = 0;
-        int end = nums.length-1;
-        int ans = -1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(target==nums[mid]){
-                ans = mid;
-                start = mid+1;
-            }
-            else if(target>nums[mid]){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
-        }
-        return ans;
-    }
+
 }
