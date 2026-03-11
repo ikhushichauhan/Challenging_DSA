07d59e78c45f84fe4ac2999319e59e70849d6600
Khushi Chauhan <2401201167@krmu.edu.in>
2026-03-11 19:42:21 +0530

    Time: 5 ms (79.2%), Space: 45.8 MB (93.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
-        int orig = x;
+        int n = x;
         long rev = 0;
-        while (x > 0) {
-            rev = rev * 10 + x % 10;
-            x = x / 10;
+        while (n > 0) {
+            int d = n%10;
+            rev = rev * 10 + d;
+            n = n / 10;
         }
-        return rev == orig;
+        if(rev == x){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
