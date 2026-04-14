d08e3753c756bb25accce9631556a2cd1697243e
Khushi Chauhan <2401201167@krmu.edu.in>
2026-04-14 16:05:04 +0530

    Time: 9 ms (98.92%), Space: 59.4 MB (61.11%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null||head.next==null) return head;
+        ListNode slow=head, fast=head, prev=null;
+    while(fast!=null&&fast.next!=null){
+            prev=slow; slow=slow.next; fast=fast.next.next;
+        }
+        prev.next=null;
+        ListNode l1=sortList(head);
+     ListNode l2=sortList(slow);
+        return merge(l1,l2);
+    }
+    private ListNode merge(ListNode l1,ListNode l2){
+        ListNode d=new ListNode(0), t=d;
+while(l1!=null&&l2!=null){
+            if(l1.val<l2.val){ t.next=l1; l1=l1.next; }
+            else{ t.next=l2; l2=l2.next; }
+            t=t.next;
+        }
+        if(l1!=null) t.next=l1;
+    if(l2!=null) t.next=l2;
+        return d.next;
+    }
+}
